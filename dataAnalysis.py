c566755acf836a8698ba3ab9c71056edba88e3e5
Sujay Nair <sujaynair03@gmail.com>
2024-06-05 11:04:37 -0400

    added iron plots

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -12,38 +12,28 @@ import fsspec
 import pdb
 pd.options.mode.copy_on_write = True
 
-def plot_histogram(df, column_name):
-    plt.figure(figsize=(10, 6))
-    df[column_name].hist(bins=30, edgecolor='black')
-    plt.title(f'Histogram of {column_name}')
-    plt.xlabel(column_name)
-    plt.ylabel('Frequency')
-    plt.savefig(f'histogram_{column_name}.png')
-    plt.show()
 
 file_path = '/Users/sujaynair/Documents/mrds-csv/mrds.csv'
 df = pd.read_csv(file_path)
 shape = df.shape
 colnames = df.columns.tolist()
-# print("Shape of the DataFrame:", shape)
-# print("Column names:", colnames)
 
 df['region'] = df['region'].fillna('NA')
-# df = df.sample(frac=0.1, random_state=1)
 
 values = {"commod1": "", "commod2": "", "commod3": ""}
 df[['commod1', 'commod2', 'commod3']] = df[['commod1', 'commod2', 'commod3']].fillna(value=values)
 df[['commod1', 'commod2', 'commod3']] = df[['commod1', 'commod2', 'commod3']].astype(str)
 df['commodities'] = df.apply(lambda x: ','.join(filter(None, [x['commod1'], x['commod2'], x['commod3']])), axis=1)
 df = df.assign(commodities=df['commodities'].str.split(',')).explode('commodities')
-# print(df['commodities'].explode().unique())
 df = df[df['commodities'] != '']
 
 local_map_path = "/Users/sujaynair/Downloads/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp"
 gdf = gpd.read_file(local_map_path)
 north_america = gdf[gdf['CONTINENT'] == 'North America']
-elem = 'Diamond'
+
+elem = 'Iron'
 elem_df = df[df['commodities'].str.strip() == elem]
+sampled_elem_df = elem_df.sample(frac=0.1, random_state=1)
 
 score_amplitude = {'A': 1, 'B': 0.8, 'C': 0.6, 'D': 0.4, 'E': 0.2}
 
@@ -57,6 +47,7 @@ z = np.zeros((grid_size, grid_size))
 pixel_size_x = (x_max - x_min) / grid_size
 pixel_size_y = (y_max - y_min) / grid_size
 
+# Define 2D arrays for each quality layer
 z_A = np.zeros((grid_size, grid_size))
 z_B = np.zeros((grid_size, grid_size))
 z_C = np.zeros((grid_size, grid_size))
@@ -72,13 +63,9 @@ for i in range(grid_size):
 
         print(f"Pixel [{i}, {j}] boundaries: Longitude ({lon_min}, {lon_max}), Latitude ({lat_min}, {lat_max})")
 
-        for _, row in elem_df.iterrows():
+        for _, row in sampled_elem_df.iterrows():
             if lon_min <= row['longitude'] < lon_max and lat_min <= row['latitude'] < lat_max:
                 print(f"Yes: ({row['score']}) quality resource at {row['site_name']}, {row['county']}, {row['state']} at ({row['longitude']}, {row['latitude']}) is in pixel [{i}, {j}]")
-                ''' SCORING BASED ON WHATS IN THE PIXEL
-                amplitude = score_amplitude.get(row['score'])
-                z[i, j] += amplitude
-                '''
                 if row['score'] == 'A':
                     z_A[i, j] += 1
                     z_B[i, j] += 1
@@ -114,9 +101,9 @@ for score, z in score_layers.items():
 
     north_america.plot(ax=ax, color='none', edgecolor='black')
 
-    ax.set_title(f'Observations of Score {score} in the Continental US ({elem} 100%)', fontsize=20)
+    ax.set_title(f'Observations of Score {score} in the Continental US ({elem} 10%)', fontsize=20)
     ax.set_xlabel('Longitude', fontsize=15)
     ax.set_ylabel('Latitude', fontsize=15)
 
-    plt.savefig(os.path.join(output_dir, f'Layer_{score}_{elem}100%.png'))
+    plt.savefig(os.path.join(output_dir, f'Layer_{score}_{elem}10%.png'))
     plt.close()
